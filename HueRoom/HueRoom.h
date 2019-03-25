9bb8df5fffd0ac597078a7319270eb4d206ddfd1
Sebastian Wolf <Basti189@users.noreply.github.com>
2019-03-25 17:57:37 +0100

    Create HueRoom.h

---
+++ HueRoom/HueRoom.h
@@ -0,0 +1,30 @@
+// HueRoom.h
+
+#ifndef _HUEROOM_h
+#define _HUEROOM_h
+
+#include <Arduino.h>
+#include <vector>
+
+class HueRoom {
+private:
+	// Store id and name
+	struct Room {
+		uint8_t id;
+		String name;
+	};
+
+	// "List" with structs of Room
+	std::vector <Room> rooms;
+
+public:
+	const static bool ON = true;
+	const static bool OFF = false;
+	const static uint8_t STATE_ALL = 0;
+	const static uint8_t STATE_ANY = 1;
+	void add(uint8_t roomID, String roomName);
+	int8_t operator [] (String roomName);
+	void clear();
+};
+
+#endif
