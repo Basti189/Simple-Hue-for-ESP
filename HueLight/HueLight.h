c54b140415d37bfb8ad12705dc80597d9be69497
Sebastian Wolf <Basti189@users.noreply.github.com>
2019-04-06 18:09:23 +0200

    Add 5 standard color temperature values

--- HueLight/HueLight.h
+++ HueLight/HueLight.h
@@ -20,11 +20,20 @@ private:
 public:
 	const static bool ON = true;
 	const static bool OFF = false;
+
 	const static uint8_t ALERT_NONE = 0;
 	const static uint8_t ALERT_SELECT = 1;
 	const static uint8_t ALERT_LSELECT = 2;
+
 	const static uint8_t EFFECT_NONE = 3;
 	const static uint8_t EFFECT_COLORLOOP = 4;
+
+	const static uint16_t CT_LIGHT_BULB = 370;
+	const static uint16_t CT_HALOGEN_LAMP = 333;
+	const static uint16_t CT_FLUORESCENT_LAMP = 250;
+	const static uint16_t CT_DAYLIGHT = 182;
+	const static uint16_t CT_COOL_WHITE = 153;
+
 	void add(uint8_t lightID, String lightName);
 	int8_t operator [] (String lightName);
 	void clear();
